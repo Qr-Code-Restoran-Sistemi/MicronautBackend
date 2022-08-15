9346f991a1d2fbf3318ee278c5246bed923d7d0a
razbon <canrazbonyali@snapbytes.com>
2022-08-15 15:18:17 +0300

    sonarcube entegrasyonu denemesi

--- src/main/java/siparis/Application.java
+++ src/main/java/siparis/Application.java
@@ -1,10 +1,12 @@
 package siparis;
 
 import io.micronaut.runtime.Micronaut;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class Application {
     public static void main(String[] args) {
         Micronaut.run(Application.class, args);
-        System.out.println("Application Cannected");
+        log.info("Application Cannected");
     }
 }
